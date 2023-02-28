23fecb568709e1232d7156c439eaf78b36047026
Juan <martin.suarez.0092@gmail.com>
2023-02-27 23:44:23 -0300

    Trabajo final

---
+++ Cv/java.js
@@ -0,0 +1,10 @@
+
+//boton para mostrar y ocultar la experiencia laboral//
+function mostrardatos(){
+    var verexp=document.getElementById("exp");
+    if ( verexp.style.display === "none"){
+    verexp.style.display = "inline";
+} else{
+    verexp.style.display = "none";
+}
+}
